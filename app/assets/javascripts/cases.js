a393b396854ba73a91e00c631c718fb9b7582c7c
Cas van der Hoven <casvanderhoven@gmail.com>
2017-03-20 22:46:54 +0100

    Add resize jQuery to align original and translations better

--- app/assets/javascripts/cases.js
+++ app/assets/javascripts/cases.js
@@ -1,3 +1,11 @@
+$.fn.extend({
+  fitTextInDiv: function () {
+    var siblingHeight = $(this).siblings('.translation').height()
+    while( siblingHeight > $(this).height() ) {
+      $(this).css('line-height', (parseFloat($(this).css('line-height')) + 0.1) + "px" );
+    }
+  }
+});
 $(document).ready(function() {
   $('#switch').on('click', function(){
     if ($(this).attr('value') == 'Show structure') {
@@ -7,6 +15,7 @@ $(document).ready(function() {
     $('.original, .translation').toggleClass( "no-background");
     $('.case-colors').toggleClass("hidden");
   });
+
   $("span").hover( function() {
     var classes = $(this).attr("class").split(' ');
     var className = "." + classes[0] + "." + classes[1];
@@ -16,4 +25,6 @@ $(document).ready(function() {
     var className = "." + classes[0] + "." + classes[1];
     $(className).css( {borderBottom: "none" } );
   });
+  $(".original").fitTextInDiv();
 });
+

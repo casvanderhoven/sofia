c9e78ed4ada985acea33d99677b82a67d2068938
Cas van der Hoven <casvanderhoven@gmail.com>
2017-03-21 21:16:04 +0100

    Add CRSF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
-  if ENV.fetch("HEROKU_APP_NAME", "").include?("staging-pr-")
-    ENV["APPLICATION_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
-  end
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
@@ -12,18 +9,6 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [ :request_id ]
   config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
-  config.active_record.dump_schema_after_migration = false
-  config.middleware.use Rack::Deflater
-  config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=31557600",
-  }
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
   config.routes = false
   config.rotate_csrf_on_sign_in = true
+  config.cookie_domain = ".casvanderhoven.me"
 end
